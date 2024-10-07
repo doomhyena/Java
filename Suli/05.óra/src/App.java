99b990719a98614ab2baaa46971e580bcf8c8e8c
Csontos Kincső <91425554+doomhyena@users.noreply.github.com>
2024-10-07 15:00:48 +0200

    NEM PRINTIN HANEM PRINTLN

--- Suli/05.óra/src/App.java
+++ Suli/05.óra/src/App.java
@@ -4,18 +4,18 @@ public class App {
     public static void main(String[] args) throws Exception {
         Random rnd =  new Random();
 
-        double a = rnd.nextInt(100-10+1)+10; //10-100
-        double b = rnd.nextInt(100-10+1)+10; //10-100
-        double c = rnd.nextInt(100-10+1)+10; //10-100
+        double a = rnd.println(100-10+1)+10; //10-100
+        double b = rnd.println(100-10+1)+10; //10-100
+        double c = rnd.println(100-10+1)+10; //10-100
 
         if (a + b > c && a + c > b && b + c > a) {
             double ker = a+b+c;
             double s = ker/2;
             double ter =  Math.sqrt(s*(s-a)*(s-b)*(s-c));
-            System.out.printIn("Kerület: "+ker);
-            System.out.printIn("Terület: "+ter);
+            System.out.println("Kerület: "+ker);
+            System.out.println("Terület: "+ter);
         } else {
-            System.out.printIn("Nincs ilyen háromszög nincs!")
+            System.out.println("Nincs ilyen háromszög nincs!")
         }
     }
 }
