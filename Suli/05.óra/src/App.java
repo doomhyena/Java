1755b3b484ecb939e320baa8adbd7c6e78544668
Csontos Kincső <91425554+doomhyena@users.noreply.github.com>
2024-10-07 14:29:47 +0200

    Generálás 10-100-ig

--- Suli/05.óra/src/App.java
+++ Suli/05.óra/src/App.java
@@ -2,6 +2,8 @@ import java.util.Random;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Random rnd =  new Random();d
+        Random rnd =  new Random();
+
+        double a = rnd.nextInt(100-10+1)+10; //10-100
     }
 }
