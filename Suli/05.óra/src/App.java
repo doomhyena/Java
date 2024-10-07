bbc621302fd2bf977869baae99a2daf071fa48a7
Csontos Kincső <91425554+doomhyena@users.noreply.github.com>
2024-10-07 15:08:17 +0200

    Négyszög & Téglalap

--- Suli/05.óra/src/App.java
+++ Suli/05.óra/src/App.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class App {
     public static void main(String[] args) throws Exception {
         Random rnd =  new Random();
-
+        // Háromszög
         double a = rnd.println(100-10+1)+10; //10-100
         double b = rnd.println(100-10+1)+10; //10-100
         double c = rnd.println(100-10+1)+10; //10-100
@@ -17,6 +17,7 @@ public class App {
         } else {
             System.out.println("Nincs ilyen háromszög nincs!")
         }
+        // Kör
         double r = rnd.nextInt(100 - 10 + 1) + 10; // 10-100
         if (r != 0) {
             double ker = 2 * r * Math.PI;
@@ -25,6 +26,21 @@ public class App {
             System.out.println("Kör Terület: " + ter);
         } else {
             System.out.println("Nincs ilyen kör nincs!");
+        }
+        // Négyszög & Téglalap
+        double a1 = rnd.nextInt(100 - 10 + 1) + 10; // 10-100
+        double b1 = rnd.nextInt(100 - 10 + 1) + 10; // 10-100
+
+        if (a1 == b1) {
+        double ker = 4 * a1;
+        double ter = a1 * a1;
+        System.out.println("Négyzet Kerület: " + ker);
+        System.out.println("Négyzet Terület: " + ter);
+        } else {
+        double ker = 2 * (a1 + b1);
+        double ter = a1 * b1;
+        System.out.println("Téglalap Kerület: " + ker);
+        System.out.println("Téglalap Terület: " + ter);
         }
     }
 }
