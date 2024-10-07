1e24b25d6d8756640a0b02e47a27b0a1c8ae9a4d
Csontos Kincső <91425554+doomhyena@users.noreply.github.com>
2024-10-07 14:34:14 +0200

    if ág

--- Suli/05.óra/src/App.java
+++ Suli/05.óra/src/App.java
@@ -5,5 +5,11 @@ public class App {
         Random rnd =  new Random();
 
         double a = rnd.nextInt(100-10+1)+10; //10-100
+        double b = rnd.nextInt(100-10+1)+10; //10-100
+        double c = rnd.nextInt(100-10+1)+10; //10-100
+
+        if (a + b > c && a + c > b && b + c > a) {
+            
+        }
     }
 }
